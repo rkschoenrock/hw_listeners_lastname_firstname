0effd9711a4bcd94f03390f2ff861183f1767345
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-07-11 18:45:52 -0400

    Change styling when image is double-clicked

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ var message = document.getElementById('message'); // defines the text area
 message.addEventListener('mousemove', function () { // when you mouse over the text area
     document.getElementById('message').value = 'Your message was reset'; // it changes text area message to "Your message was reset"
 });
+
+var image = document.getElementsByTagName('img')[0]; // defines <img>
+var container = document.getElementsByClassName('container')[0]; // defines .container
+
+image.addEventListener('dblclick', function () { // when <img> is double-clicked
+    container.style.backgroundColor = 'linen'; // .container background changes to "whitesmoke" color
+});
