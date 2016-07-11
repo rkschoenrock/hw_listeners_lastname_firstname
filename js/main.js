d531d7f7d48656973459cde3ab5de0bf757b01a5
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-07-11 18:45:10 -0400

    Change textarea when hovered over

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 document.addEventListener('scroll', function () { //when you scroll through the page
     document.getElementById('message').value = 'I changed your message'; //the text area field fills in with "I changed your message"
 });
+
+var message = document.getElementById('message'); // defines the text area
+
+message.addEventListener('mousemove', function () { // when you mouse over the text area
+    document.getElementById('message').value = 'Your message was reset'; // it changes text area message to "Your message was reset"
+});
