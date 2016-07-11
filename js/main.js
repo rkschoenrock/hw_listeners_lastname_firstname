5fa91bb670385e37927fc4f1983678cce4a3497e
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-07-11 18:44:13 -0400

    Change textarea input with scroll

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener('scroll', function () { //when you scroll through the page
+    document.getElementById('message').value = 'I changed your message'; //the text area field fills in with "I changed your message"
+});
